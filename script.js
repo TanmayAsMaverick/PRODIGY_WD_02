7efde951533e52c1197c55be95e4d6c69de39948
TanmayAsMaverick <130640737+TanmayAsMaverick@users.noreply.github.com>
2024-01-06 12:08:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var [milseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+var display = document.querySelector('.display');
+var stopwatch = null;
+var lapButton = document.querySelector('.lap');
+var lapTimesList = document.querySelector('.lap-times');
+
+lapButton.addEventListener('click', () => {
+    const lapTime = display.textContent;
+    const lapItem = document.createElement('li');
+    lapItem.textContent = lapTime;
+    lapItem.classList.add('lap-time'); // Add class for lap times
+    lapTimesList.appendChild(lapItem);
+});
+
+const start = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+const reset = document.querySelector('.reset');
+
+start.addEventListener('click', () => {
+    if (stopwatch !== null) {
+        clearInterval(stopwatch);
+    }
+    stopwatch = setInterval(startButton, 10);
+});
+
+pause.addEventListener('click', () => {
+    clearInterval(stopwatch);
+});
+
+reset.addEventListener('click', () => {
+    [seconds, minutes, hours] = [0, 0, 0];
+    display.innerHTML = '00 : 00 : 00 : 00';
+    clearInterval(stopwatch);
+
+    // Clear prior lap times
+    lapTimesList.innerHTML = '';
+});
+
+function startButton() {
+    milseconds += 1;
+    if (milseconds == 100) {
+        milseconds = 0;
+        seconds++;
+
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+
+            if (minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    var h = hours < 10 ? "0" + hours : hours;
+    var m = minutes < 10 ? "0" + minutes : minutes;
+    var s = seconds < 10 ? "0" + seconds : seconds;
+    var ms = milseconds < 10 ? "0" + milseconds : milseconds;
+
+    display.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+}
